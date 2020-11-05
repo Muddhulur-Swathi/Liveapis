dba63cd57e065aa904a9a50182e1524d397299e0
Muddhulur-Swathi <swathiraju27@gmail.com>
2020-11-05 18:14:29 +0530

    liveapis

--- Apis/apiparams.js
+++ Apis/apiparams.js
@@ -85,18 +85,21 @@ app.get('/restaurantdetails/:id',(req,res)=>{
 //restaurants on meal type
 app.get('/restaurant/:mealtype',(req,res)=>{
     var query={};
+    var sort={cost:1}
     if(req.query.cuisine){
         query={"type.mealtype":req.params.mealtype,"Cuisine.cuisine":req.query.cuisine}
     }else if(req.query.city){
         query={"type.mealtype":req.params.mealtype,city:req.query.city}
     }else if(req.query.lcost && req.query.hcost){
         query={"type.mealtype":req.params.mealtype,cost:{$lt:Number(req.query.hcost),$gt:Number(req.query.lcost)}}
-        console.log(query)
+    }else if(req.query.sort){
+        query={"type.mealtype":req.params.mealtype}
+        sort={cost:Number(req.query.sort)}
     }
     else{
         query={"type.mealtype":req.params.mealtype}
     }
-    db.collection('restaurants').find(query).toArray((err,result)=>{
+    db.collection('restaurants').find(query).sort(sort).toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
